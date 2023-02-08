8cb064c1421244a12d2de2954204c818cf09841c
v_lqiaolei <v_lqiaolei@tencent.com>
2023-02-08 18:20:25 +0800

    index

--- cmds/gopartsrv/main.go
+++ cmds/gopartsrv/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	log "github.com/sirupsen/logrus"
-	"gopartsrv/public/consts"
 	"gopartsrv/public/service"
 	_ "gopartsrv/utils/logs"
 )
@@ -15,24 +14,24 @@ func main() {
 	//	mchCertificateSerialNumber string = "4F59EB541378CC84423AE305A596041C776967A1"  // 商户证书序列号
 	//	mchAPIv3Key                string = "mADUjnHmfgTtqFk1rPvZJtBcmcqQ6C09"          // 商户APIv3密钥
 	//)
-	url := "https://api.mch.weixin.qq.com/v3/pay/transactions/jsapi"
-	data := `{
-	"mchid": "1637163088",
-	"out_trade_no": "1217752501201407033233368318",
-	"appid": "wxdace645e0bc2cXXX",
-	"description": "Image形象店-深圳腾大-QQ公仔",
-	"notify_url": "https://www.weixin.qq.com/wxpay/pay.php",
-	"amount": {
-		"total": 1,
-		"currency": "CNY"
-	},
-	"payer": {
-		"openid": "o4GgauInH_RCEdvrrNGrntXDuXXX"
-	}
-}`
-	res := consts.HttpPost(url, data, "")
-
-	log.Printf("res: %v\n", res)
+	//url := "https://api.mch.weixin.qq.com/v3/pay/transactions/jsapi"
+	//data := `{
+	//	"mchid": "1637163088",
+	//	"out_trade_no": "1217752501201407033233368318",
+	//	"appid": "wxdace645e0bc2cXXX",
+	//	"description": "Image形象店-深圳腾大-QQ公仔",
+	//	"notify_url": "https://www.weixin.qq.com/wxpay/pay.php",
+	//	"amount": {
+	//		"total": 1,
+	//		"currency": "CNY"
+	//	},
+	//	"payer": {
+	//		"openid": "o4GgauInH_RCEdvrrNGrntXDuXXX"
+	//	}
+	//}`
+	//res := consts.HttpPost(url, data, "")
+	//
+	//log.Printf("res: %v\n", res)
 	log.WithFields(log.Fields{"info": "这是golang日志框架--logrus"}).Info("描述信息为golang日志框架logrus的学习")
 	service.Serviceinit() //启动服务
 }
---
+++ condition/model/order.go
@@ -0,0 +1,44 @@
+package model
+
+import (
+	"gopartsrv/utils/db"
+)
+
+//获取连接
+func init() {
+	dbs = db.DBTeamMap["work"] //同一个db
+}
+
+//订单
+type Order struct {
+	Id         string `json:"id"` //banner的id
+	Userid   int `json:"userid"`
+	Openid string `json:"openid"`
+	Amount float64 `json:"amount"`
+	Createtime   string `json:"createtime"` //创建时间
+	Updatetime string `json:"updatetime"`
+	Deletetime string `json:"deletetime"`
+}
+//获取表名
+func (b *Order) TableName() string {
+	return "order"
+}
+
+//查询内容(id 查询)
+func (u *Order) Find() (*Order, error) {
+	list := &Order{}
+	sqls := dbs.Table(u.TableName())
+	if u.Id != "" {
+		sqls = sqls.Where("id = ?", u.Id)
+	}
+	if u.Openid != "" {
+		sqls = sqls.Where("openid = ?", u.Openid)
+	}
+	err := sqls.Find(list).Limit(1).Error
+	if err != nil {
+		return &Order{}, err
+	}
+	return list, nil
+}
+
+
