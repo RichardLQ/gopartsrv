6b3b5cded2af8adecda5c9406a546c5ddd97c7ab
v_lqiaolei <v_lqiaolei@tencent.com>
2023-03-22 21:56:14 +0800

    upload images

--- condition/controller/index/index.go
+++ condition/controller/index/index.go
@@ -12,6 +12,7 @@ import (
 	"gopartsrv/condition/model"
 	"gopartsrv/public/consts"
 	"gopartsrv/utils/mini"
+	"gopartsrv/utils/qiniu"
 	"net/http"
 	"strconv"
 	"time"
@@ -204,6 +205,41 @@ func getTimes(filePath string) (bool,string) {
 		return true ,token
 	}
 	return false,""
+}
+
+
+//上传图片
+func UploadImage(c *gin.Context)  {
+	file, err := c.FormFile("file")
+	openid := c.PostForm("openid")
+	types,_:=strconv.Atoi(c.PostForm("types"))
+	if openid == ""  {
+		c.JSON(http.StatusOK, gin.H{"ret":"查询失败","data":"","err":"参数缺失","code":consts.PARAM_LACK})
+		return
+	}
+	if err != nil {
+		c.String(500, "上传图片出错")
+	}
+	str1,_,_,err:=qiniu.QiNiu_SourceUploadFile(file,"totoro","")
+	if err !=nil {
+		c.JSON(http.StatusOK, gin.H{"ret":"上传失败","data":"","err":err,"code":-1})
+		return
+	}
+	if str1 != "" {
+		pic := model.UploadFile{
+			Openid: openid,
+			Address: str1,
+			Type: types,
+			Createtime: time.Now().Format(consts.FORMATDATELONG),
+		}
+		err:=pic.Create()
+		if err !=nil {
+			c.JSON(http.StatusOK, gin.H{"ret":"上传失败","data":"","err":err,"code":-2})
+			return
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{"ret":"成功","data":str1,"err":"","code":http.StatusOK})
+	return
 }
 
 //Sign 加密信息
